b2b87589be7bb0c2bfbe08681111b0b67a28e272
Parsa-mh <mahmoodiparsa654@gmail.com>
2024-02-10 20:51:44 +0330

    header

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+window.addEventListener("load",()=>{
+    document.querySelector("#preloader").classList.add("d-none")
+})
+/*=============== SEARCH BAR JS ===============*/
+const searchBar = document.querySelector("#search-bar")
+
+const collapsePart = document.querySelector(".collapse-part")
+const collapseButton = document.querySelector("#mobile-items")
+const searchInput =document.querySelector("#search-desktop")
+const searchBarMob = document.querySelector("#search-br")
+
+const toggleSearchMob = () => {
+    searchBarMob.classList.toggle("show-search")
+}
+const toggleSearch = () =>{
+    searchBar.classList.toggle("show-search")
+}
+function collapse(){
+    document.querySelector(".mobile-version").setAttribute("style","margin-top: 0;")
+}
+function closenav (){
+    document.querySelector(".mobile-version").setAttribute("style","margin-top: -1900px;")
+}
+window.addEventListener('resize',()=>{
+    if(window.innerWidth <= 450){
+        searchInput.classList.add("d-none")
+        document.querySelector(".search-mobile").classList.remove("d-none")
+    }
+    else{
+        searchInput.classList.remove("d-none")
+        document.querySelector(".search-mobile").classList.add("d-none")
+    }
+})
+// mohkam kari
+if(window.innerWidth <= 450){
+    searchInput.classList.add("d-none")
+    document.querySelector(".search-mobile").classList.remove("d-none")
+}
+else{
+    searchInput.classList.remove("d-none")
+    document.querySelector(".search-mobile").classList.add("d-none")
+}
+
+        
