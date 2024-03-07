f98ae28bb35b2ffe581720bd040fe1ada9033413
Parsa-mh <mahmoodiparsa654@gmail.com>
2024-03-07 20:46:29 +0330

    initail commit

---
+++ app.js
@@ -0,0 +1,12 @@
+$("#lightSlider").lightSlider({
+  rtl: false,
+  autoWidth: true,
+  loop: true,
+  pager: false,
+  auto: true,
+  pause: 2500,
+  freeMOVE: true,
+  enableTouch: true,
+  enableDrag: true,
+  slideMargin: 15,
+});
